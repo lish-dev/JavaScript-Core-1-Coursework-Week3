3894582cc25d7e3858230bfd2413c1c950d4b106
Miss <emesquitta73@gmail.com>
2020-12-26 16:39:00 +0000

    mandatory 1 completed

--- 2-mandatory/1-oxygen-levels.js
+++ 2-mandatory/1-oxygen-levels.js
@@ -11,7 +11,10 @@
     Some string methods that might help you here are .replace() and .substring(). 
 */
 
-function safeLevels() {}
+function safeLevels(item){
+  return item.find(num => parseFloat(num)> 19.5 && parseFloat(num) < 23.5);
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
